3d4a39fc760566dad001c352b6de9955a9ab1d4f
hrushi <hrushi@hrushi-PC>
2021-10-27 16:16:22 +0530

    firstcommit

--- testgithub/src/testgit/gittest.java
+++ testgithub/src/testgit/gittest.java
@@ -4,7 +4,45 @@ public class gittest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		package POM_Classes;
 
+		import org.openqa.selenium.WebDriver;
+		import org.openqa.selenium.WebElement;
+		import org.openqa.selenium.support.FindBy;
+		import org.openqa.selenium.support.PageFactory;
+
+		public class Login_Kite {
+
+			@FindBy(xpath = "//input[@id = 'userid']")
+		    private WebElement username;
+			
+			@FindBy(xpath = "//input[@id = 'password']")
+			private WebElement password;
+			
+			@FindBy(xpath = "//button[@type= 'submit']")
+			private WebElement login;
+			
+			
+			public Login_Kite(WebDriver driver) {
+				
+				PageFactory.initElements(driver, this);
+				
+			}
+			
+			
+			
+		}
+
+
+
+
+
+
+
+
+
+
+		
 	}
 
 }
